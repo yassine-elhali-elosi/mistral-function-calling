e3aa9a4d0641bdd9afbcb8463a90fefe0cbb0c72
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-05-21 14:00:00 +0200

    database example with functions

---
+++ database.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import json
+import functools
+
+data = {
+    'transaction_id': ['T1001', 'T1002', 'T1003', 'T1004', 'T1005'],
+    'customer_id': ['C001', 'C002', 'C003', 'C002', 'C001'],
+    'payment_amount': [125.50, 89.99, 120.00, 54.30, 210.20],
+    'payment_date': ['2021-10-05', '2021-10-06', '2021-10-07', '2021-10-05', '2021-10-08'],
+    'payment_status': ['Paid', 'Unpaid', 'Paid', 'Paid', 'Pending']
+}
+
+df = pd.DataFrame(data)
+
+def retrieve_payment_status(df: data, transaction_id: str) -> str:
+    if transaction_id in df.transaction_id.values: 
+        return json.dumps({'status': df[df.transaction_id == transaction_id].payment_status.item()})
+    return json.dumps({'error': 'transaction id not found.'})
+
+def retrieve_payment_date(df: data, transaction_id: str) -> str:
+    if transaction_id in df.transaction_id.values: 
+        return json.dumps({'date': df[df.transaction_id == transaction_id].payment_date.item()})
+    return json.dumps({'error': 'transaction id not found.'})
+
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "retrieve_payment_status",
+            "description": "Get payment status of a transaction",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "transaction_id": {
+                        "type": "string",
+                        "description": "The transaction id.",
+                    }
+                },
+                "required": ["transaction_id"],
+            },
+        },
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "retrieve_payment_date",
+            "description": "Get payment date of a transaction",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "transaction_id": {
+                        "type": "string",
+                        "description": "The transaction id.",
+                    }
+                },
+                "required": ["transaction_id"],
+            },
+        },
+    }
+]
+
+names_to_functions = {
+    'retrieve_payment_status': functools.partial(retrieve_payment_status, df=df),
+    'retrieve_payment_date': functools.partial(retrieve_payment_date, df=df)
+}
+
+messages = [{"role": "user", "content": "What's the status of my transaction T1001"}]
