e9c0e401cc604e4dcb2385340064853e07a261b7
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-05-21 14:18:00 +0200

    better output

--- main.py
+++ main.py
@@ -26,3 +26,16 @@ print("\nfunction_name: ", function_name, "\nfunction_params: ", function_params
 
 function_result = names_to_functions[function_name](**function_params)
 print(function_result)
+
+messages.append({
+    "role": "tool",
+    "name": function_name,
+    "content": function_result,
+    "tool_call_id": tool_call.id
+})
+
+response = client.agents.complete(
+    agent_id="ag:7e1f4155:20250521:untitled-agent:7404ab45",
+    messages=messages
+)
+print(response.choices[0].message.content)
