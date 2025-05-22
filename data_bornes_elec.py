9810819ae89eb3fbdaaf77ef8738873c8eb221ba
yassine-elhali-elosi <yassine.elhali@elosi.com>
2025-05-22 17:07:13 +0200

    function calling with charging stations api

--- data_bornes_elec.py
+++ data_bornes_elec.py
@@ -8,18 +8,18 @@ r = requests.get("https://tabular-api.data.gouv.fr/api/resources/eb76d20a-8501-4
 data = r.json()["data"]
 data_df = pd.DataFrame(data)
 
-def retrieve_minimum_charging_points_number(df: data_df, nb: int) -> object:
+def retrieve_minimum_charging_points_number(df: data_df, nbre_pdc: int) -> object:
     results = []
     for i in range(len(data_df)):
         element = data_df.iloc[i]
-        if element["nbre_pdc"] >= nb:
+        if element["nbre_pdc"] >= nbre_pdc:
             results.append(element)
 
     if len(results) == 0:
         return json.dumps({'error': 'No charging points found with the specified number.'})
-    print(results)
-    print(len(results))
-    return results
+    #print(results)
+    #print(len(results))
+    return json.dumps({'count': len(results)})
 
 bornes_elec_tools = [
     {
